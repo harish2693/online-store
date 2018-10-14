24b0627049ea25e1f0eff297b6b8e62ab7c946c6
Harish <harish@Harishs-MacBook-Pro.local>
2018-10-14 21:18:07 +0530

    Initial commit

---
+++ src/main/java/com/specbee/store/repository/InvoiceRepository.java
@@ -0,0 +1,23 @@
+package com.specbee.store.repository;
+
+import com.specbee.store.domain.Invoice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+/**
+ * Spring Data  repository for the Invoice entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    Page<Invoice> findAllByOrderCustomerUserLogin(String login, Pageable pageable);
+
+    Optional<Invoice> findOneByIdAndOrderCustomerUserLogin(Long id, String login);
+
+}
---
+++ src/main/java/com/specbee/store/repository/OrderItemRepository.java
@@ -0,0 +1,23 @@
+package com.specbee.store.repository;
+
+import com.specbee.store.domain.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+/**
+ * Spring Data  repository for the OrderItem entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    Page<OrderItem> findAllByOrderCustomerUserLogin(String login, Pageable pageable);
+
+    Optional<OrderItem> findOneByIdAndOrderCustomerUserLogin(Long id, String login);
+
+}
